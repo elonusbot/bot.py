6a19fc2ec972f08c4f4f521721362ae7f8a659f0
elonus <andreas.matre@gmail.com>
2016-04-18 21:27:31 +0200

    modified connect.py
    Signed-off-by: elonus <andreas.matre@gmail.com>

--- connect.py
+++ connect.py
@@ -1,8 +1,20 @@
+from time import sleep
+import socket
+
 network = "chat.freenode.net"
 port = 6697
+nick = "ElonusBot"
+description = nick
+nickServUsername = "elonusbot"
+nickServPass = "gutta4197"
 
-def connect(ircNetwork, ircPort):
+def connect(ircNetwork, ircPort, nick, nickServUsername, nickServPass):
     irc = socket.socket ( socket.AF_INET, socket.TCP_NODELAY )
     irc.connect( ( network, port) )
     data = data(irc.recv (4096) )
     print(data.getRawData())
+    irc.send( ( "NICK " + nick + "\r\n").encode('utf-8') )
+    irc.send( ( "USER " + nick + " " + nick + " " + nick + " :" + description + "\r\n").encode('utf-8') )
+    sleep(2)
+    irc.send( ( "PRIVMSG NickServ: identify " + nickServUsername + ' ' + nickServPass + '\r\n').encode('utf-8') )
+    data = data(irc.recv (4096) )
--- ping.py
+++ ping.py
@@ -1,2 +1,2 @@
 def ping(server):
-    return "PONG" + str(server)
+    return "PONG " + str(server)
